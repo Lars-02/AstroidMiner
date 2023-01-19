953ca7f4c7902d4b99b6deceb387a4df66a46e9b
Maik de Kruif <maik@maik.dev>
2023-01-19 04:20:46 +0100

    Connect neighbours from parsers

--- src/main/java/factories/GalaxyBuilder.java
+++ src/main/java/factories/GalaxyBuilder.java
@@ -2,19 +2,41 @@ package factories;
 
 import models.Entity;
 import models.Galaxy;
+import models.Planet;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class GalaxyBuilder {
     private final Galaxy galaxy;
+    private final Map<Planet, List<String>> planetNeighbours;
 
     public GalaxyBuilder() {
         this.galaxy = new Galaxy();
+        this.planetNeighbours = new HashMap<>();
     }
 
-    public void addEntity(Entity entity) {
+    public void addEntity(Entity entity, List<String> neighbours) {
         galaxy.addEntity(entity);
+
+        if (entity instanceof Planet)
+            this.planetNeighbours.put((Planet) entity, neighbours);
     }
 
     public Galaxy getResult() {
+        planetNeighbours.forEach((planet, neighbourNames) -> {
+            for (var neighbourName : neighbourNames) {
+                planetNeighbours
+                        .keySet()
+                        .stream()
+                        .filter(entity2 -> entity2.name.equals(neighbourName))
+                        .findFirst()
+                        .get()
+                        .addNeighbour(planet);
+            }
+        });
         return this.galaxy;
     }
 }
